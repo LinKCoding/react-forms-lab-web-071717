e366340d3f93150076e7e7de5587626f205729c0
linkcoding <kenny.lin.91@gmail.com>
2017-09-19 23:44:50 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,41 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUserChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  login = (event) => {
+    event.preventDefault()
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.login}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.handleUserChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handlePasswordChange} />
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,22 +4,47 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      poem: event.target.value
+    })
+    this.poemChecker()
+  }
+
+
+  poemChecker = () => {
+    var lines = this.state.poem.split(/\r*\n/)
+    if(lines.length !== 3) {
+      return false
+    } else if (lines[0].trim().split(' ').length !== 5){
+      return false
+    } else if (lines[1].trim().split(' ').length !== 3) {
+      return false
+    } else if (lines[2].trim().split(' ').length !== 5) {
+      return false
+    } else {
+      return true
+    }
   }
 
   render() {
     return (
       <div>
-        <textarea 
-          rows="3" 
-          cols="60" 
+        <textarea onChange={this.handleChange}
+          rows="3"
+          cols="60"
         />
-        <div 
-          id="poem-validation-error" 
+        {this.poemChecker() ? null : <div
+          id="poem-validation-error"
           style={{color: 'red'}}
         >
-          This poem is not written in the right structure!
-        </div>
+           This poem is not written in the right structure!
+        </div> }
       </div>
     );
   }
